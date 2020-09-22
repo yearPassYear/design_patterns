d855fb4d11e71996b08b5db4c0b14fc58b324cbc
谭青廉 <tanql@bonree.com>
2020-09-22 11:31:25 +0800

    简单工厂模式demo

--- src/main/java/com/yearPassYear/designPatterns/factory/ChartFactory.java
+++ src/main/java/com/yearPassYear/designPatterns/factory/ChartFactory.java
@@ -11,7 +11,12 @@ import org.apache.commons.lang.StringUtils;
  * @author tanql
  * @version 1.5.0
  * @date 2020/9/22 11:11
- * @desc  简单工厂模式   适用场景：多个复杂对象创建
+ * @desc  简单工厂模式
+ * 解决问题：解决多个接口选择问题
+ * 如何使用：根据不同条件创建不同子类实例
+ * 实现方法：子类实现工厂接口,返回抽象产品
+ *  适用场景：多个复杂对象创建，根据类型不同作不同处理，当前demo：根据不同图形类型chartType，实例化不同工厂子类调用不同方法获得不同图形数据
+ *
  */
 public class ChartFactory {
 
@@ -33,9 +38,13 @@ public class ChartFactory {
 
 
     public static void main(String[] args) {
-        System.out.println(new ChartFactory().instanceProcesser("bar").excuteChartData());
-        System.out.println( new ChartFactory().instanceProcesser("line").excuteChartData());
-        System.out.println(new ChartFactory().instanceProcesser("pie").excuteChartData());
-        System.out.println(new ChartFactory().instanceProcesser("rader").excuteChartData());
+        ChartProcesser barProcesser = new ChartFactory().instanceProcesser("bar");
+        ChartProcesser lineProcesser = new ChartFactory().instanceProcesser("line");
+        ChartProcesser pieProcesser = new ChartFactory().instanceProcesser("pie");
+        ChartProcesser raderProcesser = new ChartFactory().instanceProcesser("rader");
+        System.out.println(barProcesser.excuteChartData());
+        System.out.println( lineProcesser.excuteChartData());
+        System.out.println(pieProcesser.excuteChartData());
+        System.out.println(raderProcesser.excuteChartData());
     }
 }
